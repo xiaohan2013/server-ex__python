c511a45a36b3856ecddc5908fa2fed6f08f992c1
xiaohan2013 <tanyongxiaohan@163.com>
2017-08-30 19:55:25 +0800

    add base concurrent

--- 01_base/concurrent.py
+++ 01_base/concurrent.py
@@ -1,7 +1,26 @@
 # -*- utf-8 -*-
-#!/usr/bin/python
+#! /usr/bin/python
 
+def consumer():
+    r = ''
+    while 1:
+        n = yield r
+        if not n:
+            break
 
-f = open('generator.py', 'r')
-print(f.read())
-f.close()
+        print("consumer receive msg: %s" % n)
+        r = "%s OK" % n
+
+def produce():
+    c = consumer()
+    c.send(None)
+
+    n = 0
+    while n < 3:
+        n += 1
+        print('produce send msg: %s' % n)
+        r = c.send(n)
+        print('consumer return msg: %s' % r)
+    c.close()
+
+produce()
